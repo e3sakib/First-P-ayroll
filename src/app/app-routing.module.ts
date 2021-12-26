f4f3916a31bdc48eec5c9e75627402067a85d992
Sakib Ahammed <satuhin6573@gmail.com>
2021-12-26 07:20:17 +0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/adminlayout/admin/dashboard/dashboard.component';
+import { ForgetPasswordComponent } from './components/adminlayout/admin/forget-password/forget-password.component';
+import { LoginComponent } from './components/adminlayout/admin/login/login.component';
+import { RecoverPasswordComponent } from './components/adminlayout/admin/recover-password/recover-password.component';
+import { RegisterComponent } from './components/adminlayout/admin/register/register.component';
+import { AdminlayoutComponent } from './components/adminlayout/adminlayout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register', component:RegisterComponent},
+  {path:'forget-password', component:ForgetPasswordComponent},
+  {path:'recover-password', component:RecoverPasswordComponent},
+  {path:'login', component:LoginComponent},
+  {path:"admin", component:AdminlayoutComponent, children:[
+    {path:"",component:DashboardComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminlayoutComponent } from './components/adminlayout/adminlayout.component';
+import { AdminfooterComponent } from './components/adminlayout/adminfooter/adminfooter.component';
+import { AdminheaderComponent } from './components/adminlayout/adminheader/adminheader.component';
+import { DashboardComponent } from './components/adminlayout/admin/dashboard/dashboard.component';
+import { LoginComponent } from './components/adminlayout/admin/login/login.component';
+import { RegisterComponent } from './components/adminlayout/admin/register/register.component';
+import { ForgetPasswordComponent } from './components/adminlayout/admin/forget-password/forget-password.component';
+import { RecoverPasswordComponent } from './components/adminlayout/admin/recover-password/recover-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminlayoutComponent,
+    AdminfooterComponent,
+    AdminheaderComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgetPasswordComponent,
+    RecoverPasswordComponent
   ],
   imports: [
     BrowserModule,
